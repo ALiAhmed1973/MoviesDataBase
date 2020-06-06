f7b26dbf26aff1d00f0abaede50c796593364231
Ali <eng_aliahmed1973@yahoo.com>
2020-06-06 13:20:43 +0200

    Add Movie Model

---
+++ app/src/main/java/com/aliprojects/moviesdatabase/model/Movie.java
@@ -0,0 +1,113 @@
+package com.aliprojects.moviesdatabase.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Movie implements Parcelable {
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+    @SerializedName("id")
+    private int movieId;
+    @SerializedName("original_title")
+    private String originalTitle;
+    @SerializedName("poster_path")
+    private String posterPath;
+    @SerializedName("overview")
+    private String movieOverview;
+    @SerializedName("vote_average")
+    private double movieRating;
+    @SerializedName("release_date")
+    private String releaseDate;
+
+    public Movie(int movieId, String originalTitle, String posterPath, String movieOverview, double movieRating, String releaseDate) {
+        this.movieId = movieId;
+        this.originalTitle = originalTitle;
+        this.posterPath = posterPath;
+        this.movieOverview = movieOverview;
+        this.movieRating = movieRating;
+        this.releaseDate = releaseDate;
+    }
+
+    protected Movie(Parcel in) {
+        movieId = in.readInt();
+        originalTitle = in.readString();
+        posterPath = in.readString();
+        movieOverview = in.readString();
+        movieRating = in.readDouble();
+        releaseDate = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(movieId);
+        dest.writeString(originalTitle);
+        dest.writeString(posterPath);
+        dest.writeString(movieOverview);
+        dest.writeDouble(movieRating);
+        dest.writeString(releaseDate);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getMovieOverview() {
+        return movieOverview;
+    }
+
+    public void setMovieOverview(String movieOverview) {
+        this.movieOverview = movieOverview;
+    }
+
+    public double getMovieRating() {
+        return movieRating;
+    }
+
+    public void setMovieRating(double movieRating) {
+        this.movieRating = movieRating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
