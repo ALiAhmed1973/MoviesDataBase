fa8be9ce35cd55b0d701ff5510a014a9f03fe254
Ali <eng_aliahmed1973@yahoo.com>
2020-06-08 16:11:22 +0200

    Add Movie Details

---
+++ app/src/main/java/com/aliprojects/moviesdatabase/MovieDetailsActivity.java
@@ -0,0 +1,38 @@
+package com.aliprojects.moviesdatabase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.aliprojects.moviesdatabase.databinding.ActivityMovieDetailsBinding;
+import com.aliprojects.moviesdatabase.model.Movie;
+import com.aliprojects.moviesdatabase.ui.MainActivity;
+
+import java.util.Objects;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    ActivityMovieDetailsBinding movieDetailsBinding;
+    Movie movie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        movieDetailsBinding = ActivityMovieDetailsBinding.inflate(getLayoutInflater());
+        View view = movieDetailsBinding.getRoot();
+        setContentView(view);
+        Intent intent = getIntent();
+        if (intent != null) {
+            Bundle bundle = intent.getBundleExtra(MainActivity.MOVIE_BUNDLE_KEY);
+            movie = Objects.requireNonNull(bundle).getParcelable(MainActivity.MOVIE_BUNDLE_KEY);
+        }
+        movieDetailsBinding.titleTv.setText(movie.getOriginalTitle());
+        movieDetailsBinding.overviewTv.setText(movie.getMovieOverview());
+        movieDetailsBinding.ratingTv.setText(String.valueOf(movie.getMovieRating()));
+        movieDetailsBinding.releasedateTv.setText(movie.getReleaseDate());
+    }
+}
--- app/src/main/java/com/aliprojects/moviesdatabase/ui/MainActivity.java
+++ app/src/main/java/com/aliprojects/moviesdatabase/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.aliprojects.moviesdatabase.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,6 +10,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.aliprojects.moviesdatabase.MovieDetailsActivity;
 import com.aliprojects.moviesdatabase.R;
 import com.aliprojects.moviesdatabase.databinding.ActivityMainBinding;
 import com.aliprojects.moviesdatabase.model.Movie;
@@ -18,6 +20,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements MovieAdapter.OnMovieClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String MOVIE_BUNDLE_KEY = "com.aliprojects.moviesdatabase.ui.movie_details";
     MovieAdapter movieAdapter;
     MovieViewModel movieViewModel;
     private ActivityMainBinding mainBinding;
@@ -52,6 +55,11 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.OnMo
 
     @Override
     public void onItemClick(Movie movie) {
+        Intent intent = new Intent(this, MovieDetailsActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(MOVIE_BUNDLE_KEY, movie);
+        intent.putExtra(MOVIE_BUNDLE_KEY, bundle);
+        startActivity(intent);
 
     }
 }
