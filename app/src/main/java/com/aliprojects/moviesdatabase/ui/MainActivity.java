7dc844a92bc92c5fe976087aec9d0e7f00daed10
Ali <eng_aliahmed1973@yahoo.com>
2020-06-09 17:35:19 +0200

    RecyclerView pos

--- app/src/main/java/com/aliprojects/moviesdatabase/ui/MainActivity.java
+++ app/src/main/java/com/aliprojects/moviesdatabase/ui/MainActivity.java
@@ -18,14 +18,17 @@ import com.aliprojects.moviesdatabase.R;
 import com.aliprojects.moviesdatabase.databinding.ActivityMainBinding;
 import com.aliprojects.moviesdatabase.model.Movie;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements MovieAdapter.OnMovieClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String MOVIE_BUNDLE_KEY = "com.aliprojects.moviesdatabase.ui.movie_details";
+    private static final String RECYCLERVIEW_STATE_BUNDLE_EXTRA = "com.aliprojects.moviesdatabase.ui.recyclerview_state";
     MovieAdapter movieAdapter;
     MovieViewModel movieViewModel;
     private ActivityMainBinding mainBinding;
-
+    private List<Movie> movies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,16 +40,20 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.OnMo
 
 
         movieViewModel = new ViewModelProvider(this).get(MovieViewModel.class);
-        movieViewModel.getMoviesDataResponse();
-        movieViewModel.getMovies().observe(this, movies -> {
-            movieAdapter.setMovieList(movies);
-        });
-
-
+        if (savedInstanceState == null) {
+            movieViewModel.getMoviesDataResponse();
+        } else {
+            //  restoreRecyclerviewScrollPosition();
+        }
         movieAdapter = new MovieAdapter(this, this);
+        movieAdapter.setStateRestorationPolicy(RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, RecyclerView.VERTICAL, false);
         mainBinding.movieRv.setLayoutManager(gridLayoutManager);
         mainBinding.movieRv.setAdapter(movieAdapter);
+
+        movieViewModel.getMovies().observe(this, movies -> {
+            movieAdapter.setMovieList(movies);
+        });
 
 
     }
@@ -82,4 +89,25 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.OnMo
         }
 
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(RECYCLERVIEW_STATE_BUNDLE_EXTRA, mainBinding.movieRv.getLayoutManager().onSaveInstanceState());
+    }
+
+
+//    @Override
+//    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//        mainBinding.movieRv.getLayoutManager().onRestoreInstanceState(savedInstanceState.getParcelable(RECYCLERVIEW_STATE_BUNDLE_EXTRA));
+//    }
+////
+//    private void restoreRecyclerviewScrollPosition()
+//    {
+//        if(layoutManagerState!=null) {
+//            mainBinding.movieRv.getLayoutManager().onRestoreInstanceState(layoutManagerState);
+//            layoutManagerState=null;
+//        }
+//    }
 }
